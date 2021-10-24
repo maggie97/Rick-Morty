62ba368bfd2bcfde8faa55f7fc0403ada59e6b72
maggie97 <maggielee@icloud.com>
2021-10-24 14:36:16 -0500

    Initial Commit

---
+++ Ricky&Morty/Ricky_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ricky_MortyApp.swift
+//  Ricky&Morty
+//
+//  Created by MaggieMendez  on 24/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Ricky_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
